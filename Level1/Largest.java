381a3a11198d3febe33b1feb930e105550a293af
Amishra078 <mishraabhijeet078@gmail.com>
2025-09-02 14:58:19 +0530

    Control Flow

---
+++ Level1/Largest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int num1 = sc.nextInt();
+        
+        int num2 = sc.nextInt();
+        
+        int num3 = sc.nextInt();
+        System.out.println("the first number the largest? " + (num1 >= num2 && num1 >= num3));
+        System.out.println("the second number the largest? " + (num2 >= num1 && num2 >= num3));
+        System.out.println("the third number the largest? " + (num3 >= num1 && num3 >= num2));
+    }
+}
