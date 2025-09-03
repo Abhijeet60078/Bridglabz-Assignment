f0d7781592242c926f04b27b4b4053dfbda42abb
Amishra078 <mishraabhijeet078@gmail.com>
2025-09-04 01:36:46 +0530

    Control Flow Level1

---
+++ src/ControlFLow/SumNaturalWhile.java
@@ -0,0 +1,26 @@
+package ControlFLow;
+
+import java.util.Scanner;
+
+public class SumNaturalWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        if (n >= 1) {
+            int sumFormula = n * (n + 1) / 2;
+
+            int sumWhile = 0, i = 1;
+            while (i <= n) {
+                sumWhile += i;
+                i++;
+            }
+
+            System.out.println("Sum using formula: " + sumFormula);
+            System.out.println("Sum using while loop: " + sumWhile);
+        } else {
+            System.out.println("Not a natural number");
+        }
+    }
+}
---
+++ src/ControlFLow/SumUntilZero.java
@@ -0,0 +1,21 @@
+package ControlFLow;
+
+import java.util.Scanner;
+
+public class SumUntilZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double total = 0.0;
+
+
+        double num = sc.nextDouble();
+
+        while (num != 0) {
+            total += num;
+
+            num = sc.nextDouble();
+        }
+
+        System.out.println("Total sum = " + total);
+    }
+}
