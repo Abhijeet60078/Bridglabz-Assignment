7c9e56e45b9fb7605f5c175df3634efb44ed16f8
Amishra078 <mishraabhijeet078@gmail.com>
2025-09-04 01:57:43 +0530

    Control Flow Level3

---
+++ src/ControlFlowLevel3/CountDigits.java
@@ -0,0 +1,19 @@
+package ControlFlowLevel3;
+
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int count = 0, temp = number;
+        while (temp != 0) {
+            temp /= 10;
+            count++;
+        }
+
+        System.out.println("Number of digits in " + number + " = " + count);
+    }
+}
